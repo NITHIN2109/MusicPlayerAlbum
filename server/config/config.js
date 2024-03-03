27143e6f45b58e8c580a3f25ac95a21aada0e9ab
NITHIN2109 <110482153+NITHIN2109@users.noreply.github.com>
2024-03-04 00:50:16 +0530

    Nithin

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 let config = {
-  host: "localhost",
-  user: "root",
-  password: "Nit123@",
-  database: "albums",
+  host: "bnijdmfasqisjyl7tjua-mysql.services.clever-cloud.com",
+  user: "usjiohh4bybb0inm",
+  password: "wXnUBZqC5mhvfJHf6A2i",
+  database: "bnijdmfasqisjyl7tjua",
 };
 
 module.exports = config;
